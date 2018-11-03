1570ccdfa2cc7e5d563657c3f2f546c945c4bee8
jayson <miles@gzsolartech.com>
2018-11-03 14:25:03 +0800

    可转换DOCUMENT XML 写入关系表

--- testdb.py
+++ testdb.py
@@ -1,11 +1,12 @@
-
-
 import cx_Oracle
 import os
 import xmltodict
 import pandas as pd
 import xml.etree.cElementTree as et
 from sqlalchemy import types, create_engine
+
+
+
 
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8' 
 
@@ -30,15 +31,18 @@ for res in resultall:
         #print(node.tag, node.text)
         col_name = node.tag
         val = node.text
+        if(val==None):
+            val = '0'
         s[col_name] = val
         #print(s)
-    print(s)
+    #print(s)
     dftemp = pd.DataFrame([s], columns=s.keys())
    # df = pd.concat([df, dftemp], axis=0, sort=False).reset_index()
     df = pd.concat([df, dftemp], sort=False)
-print(df)
-#conn = create_engine('oracle+cx_oracle://csmart:csmart@mail.gzsolartech.com:1521/?service_name=smartformsdb')
-#dtyp = {c:types.VARCHAR(df[c].str.len().max())
-#        for c in df.columns[df.dtypes == 'object'].tolist()}
-#df.to_sql('jaysontest', conn, dtype=dtyp, if_exists='replace', chunksize=50)
+#print(df)
+conn = create_engine('oracle+cx_oracle://csmart:csmart@mail.gzsolartech.com:1521/?service_name=smartformsdb')
+dtyp = {c:types.VARCHAR(df[c].str.len().max())
+        for c in df.columns[df.dtypes == 'object'].tolist()}
+#print(dtyp)
+df.to_sql('jaysontest', conn, dtype=dtyp, if_exists='replace', chunksize=50)
 
