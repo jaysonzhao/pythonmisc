d0066d63ccf1a11b0fc3ed99a731330c54ebecea
jayson <miles@gzsolartech.com>
2018-11-12 20:34:41 +0800

    add szgas todo test 3 portal server random access

--- szgastodotest.py
+++ szgastodotest.py
@@ -1,22 +1,36 @@
 import requests
 import threading
 import time
+import random
 
 
 def getUrlA():
-    url = 'http://10.9.6.44:10039/wps/portal/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziDVCAo4FTkJGTsYGBu7OJfjgWBchK9aNI14-iIAq_8V54LbAwAPnAqMjX2TddP6ogsSRDNzMvLV8_Iic_Pb-0BGh7FCH9UYRcEOiTn56emuIPNK4gNzSiyictOCBdUREA1Ejwcg!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/p0/IZ7_00000000000000A0BR2B300I81=CZ6_00000000000000A0BR2B300GC4=LA0=Eaction!wps.portlets.login==/#Z7_00000000000000A0BR2B300I81'
+    rannum = random.randint(0, 2)
+    url = ['http://10.9.6.44:10039',
+           'http://10.9.6.45:10059',
+           'http://10.9.6.43:10059']
+
+    loginurlstr = url[rannum]+"/wps/portal/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziDVCAo4FTkJGTsYGBu7OJfjgWBchK9aNI14-iIAq_8V54LbAwAPnAqMjX2TddP6ogsSRDNzMvLV8_Iic_Pb-0BGh7FCH9UYRcEOiTn56emuIPNK4gNzSiyictOCBdUREA1Ejwcg!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/p0/IZ7_00000000000000A0BR2B300I81=CZ6_00000000000000A0BR2B300GC4=LA0=Eaction!wps.portlets.login==/#Z7_00000000000000A0BR2B300I81"
+    requrlstr = url[rannum]+"/wps/myportal/Todo/todo/findAll2.action?perPage=500&currentPage=1"
     d = {'wps.portlets.userid': 'SONGSS', 'password': 'Songsisi1989'}
-    r = requests.post(url, data=d, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-    r1 = requests.get('http://10.9.6.44:10039/wps/myportal/Todo/todo/findAll2.action?perPage=500&currentPage=1')
-    print(r1.text)
+    r = requests.post(loginurlstr, data=d, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    r1 = requests.get(requrlstr)
+    print(requrlstr,r1.text)
 
 
 def getUrlB():
-    url = 'http://10.9.6.44:10039/wps/portal/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziDVCAo4FTkJGTsYGBu7OJfjgWBchK9aNI14-iIAq_8V54LbAwAPnAqMjX2TddP6ogsSRDNzMvLV8_Iic_Pb-0BGh7FCH9UYRcEOiTn56emuIPNK4gNzSiyictOCBdUREA1Ejwcg!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/p0/IZ7_00000000000000A0BR2B300I81=CZ6_00000000000000A0BR2B300GC4=LA0=Eaction!wps.portlets.login==/#Z7_00000000000000A0BR2B300I81'
+    rannum = random.randint(0, 2)
+    url = ['http://10.9.6.44:10039',
+           'http://10.9.6.45:10059',
+           'http://10.9.6.43:10059']
+
+    loginurlstr = url[rannum] + "/wps/portal/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziDVCAo4FTkJGTsYGBu7OJfjgWBchK9aNI14-iIAq_8V54LbAwAPnAqMjX2TddP6ogsSRDNzMvLV8_Iic_Pb-0BGh7FCH9UYRcEOiTn56emuIPNK4gNzSiyictOCBdUREA1Ejwcg!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/p0/IZ7_00000000000000A0BR2B300I81=CZ6_00000000000000A0BR2B300GC4=LA0=Eaction!wps.portlets.login==/#Z7_00000000000000A0BR2B300I81"
+    requrlstr = url[rannum] + "/wps/myportal/Todo/todo/findAllSum.action"
     d = {'wps.portlets.userid': 'SONGSS', 'password': 'Songsisi1989'}
-    r = requests.post(url, data=d, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-    r2 = requests.get('http://10.9.6.44:10039/wps/myportal/Todo/todo/findAllSum.action')
-    print(r2.text)
+    r = requests.post(loginurlstr, data=d, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    r2 = requests.get(requrlstr)
+    print(requrlstr,r2.text)
+
 
 
 if __name__ == '__main__':
